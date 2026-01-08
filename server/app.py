73ac696b6bd0a709a0a0de742d473cae1e0d0f19
Wise Munene Mutuku <wisemunene99@gmail.com>
2026-01-08 22:13:10 +0300

    done

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ class Signup(Resource):
     def post(self):
         json = request.get_json()
         user = User(
-            username=json['username']
+            username=json['username'] 
         )
         user.password_hash = json['password']
         db.session.add(user)
@@ -28,16 +28,36 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id') # Get user_id from session
+        if user_id:
+            user =User.query.get(user_id) # Query the User model
+            return user.to_dict(), 200
+        else:
+            return {}, 204 # No content if no user_id in session
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json() 
+        user = User.query.filter_by(username=data['username']).first() # Query User by username
+        if user and user.authenticate(data['password']): # Check password
+            session['user_id'] = user.id # Set user_id in session
+            return user.to_dict(), 200
+        else:
+            return {'error': 'Invalid credentials'}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None # Clear user_id from session
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
